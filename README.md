98e64cc761b1189a1b6cc177bf750abea867669f
Ian Ibbotson <ian.ibbotson@gmail.com>
2017-12-03 12:13:40 +0000

    set up readme and dev scripts area. Boilerplate dev script added

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+Devops scripts for the semweb cap filtered alert hub
---
+++ dev/full_reset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+This script resets the dev environment to a pristine state, and configures the the app to run in default-disabled mode, where all feeds will be switched off by default.
+
+This allows devs to selectively switch on individual feeds that need testing.
