f6476cac47d7671f655f5a765f452a1fd78c5373
Ian Ibbotson <ian.ibbotson@gmail.com>
2017-12-03 14:07:10 +0000

    updated

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 
 
 Devops scripts for the semweb cap filtered alert hub
+
+
+All the scripts are predicated on having a pgsql user with the following kind of setup::
+
+CREATE USER fiaadmin with CREATEDB CREATEUSER password 'your_local_fiaadmin_password_here';
+
---
+++ dev/reset.sql
@@ -0,0 +1,7 @@
+DROP DATABASE feedfacadedev;
+DROP DATABASE capcollatordev;
+CREATE DATABASE feedfacadedev;
+CREATE DATABASE capcollatordev;
+GRANT ALL PRIVILEGES ON DATABASE capcollatordev to capcollator;
+GRANT ALL PRIVILEGES ON DATABASE feedfacadedev to feedfacade;
+
