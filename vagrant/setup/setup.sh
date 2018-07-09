ca07d433bb96fe2b56e2da01126bc1afa76c2cbd
Ian Ibbotson <ianibbo@gmail.com>
2018-07-09 13:12:30 +0100

    Working on improving the ES update

--- vagrant/setup/setup.sh
+++ vagrant/setup/setup.sh
@@ -2,10 +2,17 @@
 
 echo This is the CAP Setup Script
 
-run curl
+echo sleep 10 - Just because the ES container has started, does not mean ES is up and running yet
+sleep 10
 
-curl -I http://elasticsearch:9200/alerts 2>/dev/null | head -n 1 | cut -d$' ' -f2
 
+echo run curl
+curl -I http://elasticsearch:9200/alerts
+
+echo Get HTTP resonse code for accessing alerts index
 export status=`curl -I http://elasticsearch:9200/alerts 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 
+echo status : -$status-
+
+echo All done - exit
 exit 0
