8ad3c2fc89a0e38833536bd642d19a50ab4c1e0b
Ian Ibbotson <ianibbo@gmail.com>
2018-12-23 11:06:36 +0000

    Added if exists clause to drop commands

--- dev/README.md
+++ dev/README.md
@@ -1,1 +1,12 @@
 This directory contains files that will run the necessary componets on a developer workstation
+
+
+clean up a dev setup with
+
+docker-compose down -v --remove-orphans 
+
+
+
+Connect to the postgres instance in the docker container with
+
+docker exec -it cap_dev_pg psql 
--- dev/init.sql
+++ dev/init.sql
@@ -3,18 +3,18 @@ select 1;
 CREATE USER capcollator WITH PASSWORD 'capcollator';
 CREATE USER feedfacade WITH PASSWORD 'feedFacade';
 
-DROP DATABASE capcollatordev;
+DROP DATABASE if exists capcollatordev;
 CREATE DATABASE capcollatordev;
 GRANT ALL PRIVILEGES ON DATABASE capcollatordev to capcollator;
 
-DROP DATABASE capcollatortest;
+DROP DATABASE if exists capcollatortest;
 CREATE DATABASE capcollatortest;
 GRANT ALL PRIVILEGES ON DATABASE capcollatortest to capcollator;
 
-DROP DATABASE feedfacadedev;
+DROP DATABASE if exists feedfacadedev;
 CREATE DATABASE feedfacadedev;
 GRANT ALL PRIVILEGES ON DATABASE feedfacadedev to feedfacade;
 
-DROP DATABASE feedfacadetest;
+DROP DATABASE if exists feedfacadetest;
 CREATE DATABASE feedfacadetest;
 GRANT ALL PRIVILEGES ON DATABASE feedfacadetest to feedfacade;
