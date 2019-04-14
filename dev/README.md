c701050d8e03886552bd28334e29f66d7c2d4dd6
Ian Ibbotson <ianibbo@gmail.com>
2019-04-14 16:05:18 +0100

    updated

--- dev/README.md
+++ dev/README.md
@@ -7,7 +7,12 @@ docker-compose down -v --remove-orphans
 
 Connect to the postgres instance in the docker container with
 
-docker exec -it cap_dev_pg psql 
+docker exec -it cap_dev_pg psql -Ucapcollator capcollatordev
+
+or
+
+docker exec -it cap_dev_pg psql -Ufeedfacade feedfacadedev
+
 
 
 If you should edit the DOCKERFILE for any dependent component, remember that they are cached and you will need to run docker-compose build to rebuild
--- dev/init.sql
+++ dev/init.sql
@@ -1,20 +1,25 @@
 select 1;
 
+CREATE USER cap WITH PASSWORD 'cap';
 CREATE USER capcollator WITH PASSWORD 'capcollator';
 CREATE USER feedfacade WITH PASSWORD 'feedFacade';
 
 DROP DATABASE if exists capcollatordev;
 CREATE DATABASE capcollatordev;
 GRANT ALL PRIVILEGES ON DATABASE capcollatordev to capcollator;
+GRANT ALL PRIVILEGES ON DATABASE capcollatordev to cap;
 
 DROP DATABASE if exists capcollatortest;
 CREATE DATABASE capcollatortest;
 GRANT ALL PRIVILEGES ON DATABASE capcollatortest to capcollator;
+GRANT ALL PRIVILEGES ON DATABASE capcollatortest to cap;
 
 DROP DATABASE if exists feedfacadedev;
 CREATE DATABASE feedfacadedev;
 GRANT ALL PRIVILEGES ON DATABASE feedfacadedev to feedfacade;
+GRANT ALL PRIVILEGES ON DATABASE feedfacadedev to cap;
 
 DROP DATABASE if exists feedfacadetest;
 CREATE DATABASE feedfacadetest;
 GRANT ALL PRIVILEGES ON DATABASE feedfacadetest to feedfacade;
+GRANT ALL PRIVILEGES ON DATABASE feedfacadetest to cap;
