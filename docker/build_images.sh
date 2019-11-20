c6079ef46c3b5d796bd9fbc938cf64dc91f79b87
Ian Ibbotson <ianibbo@googlemail.com>
2019-11-20 12:49:09 +0000

    updated

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -4,4 +4,14 @@ docker build -t semweb/caphub_tomcat:v1.3 -t semweb/caphub_tomcat:v1 -t semweb/c
 docker push semweb/caphub_tomcat:v1.3
 docker push semweb/caphub_tomcat:v1
 docker push semweb/caphub_tomcat:latest
+cd rabbitmq
+docker build -t semweb/caphub_rabbit:v1.0 -t semweb/caphub_rabbit:v1 -t semweb/caphub_rabbit:latest .
+docker push semweb/caphub_rabbit:v1.0
+docker push semweb/caphub_rabbit:v1
+docker push semweb/caphub_rabbit:latest
+cd setup
+docker build -t semweb/caphub_setup:v1.0 -t semweb/caphub_setup:v1 -t semweb/caphub_setup:latest .
+docker push semweb/caphub_setup:v1.0
+docker push semweb/caphub_setup:v1
+docker push semweb/caphub_setup:latest
 
