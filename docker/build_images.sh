8a59b080a9f3e65c0d23160de54f0b6c188fa1ad
Ian Ibbotson <ianibbo@googlemail.com>
2019-11-21 16:58:53 +0000

    add nginx to swarm config file

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,9 +1,9 @@
 docker login
-cd tomcat
-docker build -t semweb/caphub_tomcat:v1.3 -t semweb/caphub_tomcat:v1 -t semweb/caphub_tomcat:latest .
-docker push semweb/caphub_tomcat:v1.3
-docker push semweb/caphub_tomcat:v1
-docker push semweb/caphub_tomcat:latest
+# cd tomcat
+# docker build -t semweb/caphub_tomcat:v1.3 -t semweb/caphub_tomcat:v1 -t semweb/caphub_tomcat:latest .
+# docker push semweb/caphub_tomcat:v1.3
+# docker push semweb/caphub_tomcat:v1
+# docker push semweb/caphub_tomcat:latest
 cd rabbitmq
 docker build -t semweb/caphub_rabbit:v1.0 -t semweb/caphub_rabbit:v1 -t semweb/caphub_rabbit:latest .
 docker push semweb/caphub_rabbit:v1.0
