fe589d0b3a20386bcb50b10570d7775ee34c1aab
Ian Ibbotson <ianibbo@gmail.com>
2020-02-01 15:09:48 +0000

    updated nginx config

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -18,8 +18,8 @@ docker push semweb/caphub_setup:v1
 docker push semweb/caphub_setup:latest
 cd ../nginx
 echo Buildig Nginx `pwd`
-docker build -t semweb/caphub_nginx:v1.0.4 -t semweb/caphub_nginx:v1.0 -t semweb/caphub_nginx:v1 -t semweb/caphub_nginx:latest .
-docker push semweb/caphub_nginx:v1.0.4
+docker build -t semweb/caphub_nginx:v1.0.6 -t semweb/caphub_nginx:v1.0 -t semweb/caphub_nginx:v1 -t semweb/caphub_nginx:latest .
+docker push semweb/caphub_nginx:v1.0.6
 docker push semweb/caphub_nginx:v1.0
 docker push semweb/caphub_nginx:v1
 docker push semweb/caphub_nginx:latest
