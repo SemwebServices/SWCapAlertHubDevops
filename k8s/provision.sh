91b1c61acb37db7dbf33127ae325acb05b95ec04
Ian Ibbotson <ianibbo@gmail.com>
2020-03-01 12:49:11 +0000

    Added service

--- k8s/provision.sh
+++ k8s/provision.sh
@@ -7,4 +7,7 @@ kubectl apply -f ./secret.yml
 ./create-configmap.sh
 kubectl apply -f statefulset-master.yml
 kubectl logs postgres-0
+kubectl apply -f postgres-service.yml
 
+# This command will allow us to connect on the host when using minikube
+# kubectl port-forward postgres-0 5432:5432
