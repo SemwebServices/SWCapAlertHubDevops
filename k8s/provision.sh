604bea7b1ca5c21a2d187eee83f21a95d76342e0
Ian Ibbotson <ianibbo@gmail.com>
2020-08-18 21:53:57 +0100

    Provisioning rabbit inside k8s sorted

--- k8s/provision.sh
+++ k8s/provision.sh
@@ -13,9 +13,33 @@ kubectl apply -f postgres-service.yml
 cd ..
 
 
-# Not working yet
-cd rabbit
-kubectl apply -f rabbit.yml
+# RabbitMQ - See https://bitnami.com/stack/rabbitmq/helm
+See also https://docs.bitnami.com/ibm/infrastructure/rabbitmq/administration/use-admin-console/
+
+This setup will create an ingress rule for hostname rabbitmq.local - arrange for that in your hosts file or other dns setup
+
+helm install semweb-default-rabbit \
+  --set ingress.enabled=true \
+  bitnami/rabbitmq -n core --create-namespace
+
+visit http://rabbitmq.local and provision an administrative user - cap/PASSWORD
+
+wget http://rabbitmq.local/cli/rabbitmqadmin
+chmod ugo+rx ./rabbitmqadmin
+
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare exchange name=FeedFetcher type=topic
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare exchange name=CAPExchange type=topic
+echo Declare queues
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare queue name=CAPCollatorATOMQueue durable=true
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare queue name=CAPCollatorRSSQueue durable=true
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare queue name=FeedFeedbackQueue durable=true
+echo Declare bindings
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare binding source="CAPExchange" destination_type="queue" destination="CAPCollatorATOMQueue" routing_key="ATOMEntry.#"
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare binding source="CAPExchange" destination_type="queue" destination="CAPCollatorRSSQueue" routing_key="RSSEntry.#"
+./rabbitmqadmin --host rabbitmq.local --port 80 --username cap --password cap declare binding source="CAPExchange" destination_type="queue" destination="FeedFeedbackQueue" routing_key="FFFeedback.#"
+
+
+
 
 cd ..
 cd es
