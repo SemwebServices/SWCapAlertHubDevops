3700ef080856e889725312f014fe34f2aa1d94c8
Ian Ibbotson <ianibbo@gmail.com>
2021-12-18 08:21:31 +0000

    Re-add proto forwarded headers - no longer adding :80 to urls in spring boot auth

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -19,8 +19,8 @@ docker push semweb/caphub_setup:v1
 docker push semweb/caphub_setup:latest
 cd ../nginx
 echo Buildig Nginx `pwd`
-docker build -t semweb/caphub_nginx:v1.0.22 -t semweb/caphub_nginx:v1.0 -t semweb/caphub_nginx:v1 -t semweb/caphub_nginx:latest .
-docker push semweb/caphub_nginx:v1.0.22
+docker build -t semweb/caphub_nginx:v1.0.23 -t semweb/caphub_nginx:v1.0 -t semweb/caphub_nginx:v1 -t semweb/caphub_nginx:latest .
+docker push semweb/caphub_nginx:v1.0.23
 docker push semweb/caphub_nginx:v1.0
 docker push semweb/caphub_nginx:v1
 docker push semweb/caphub_nginx:latest
